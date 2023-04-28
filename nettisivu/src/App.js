3a0ebbf097d35b1ec35d313ad00973baf5a9634f
Klicer <51763508+Klicer@users.noreply.github.com>
2023-04-28 16:32:10 +0300

    Update website: add background image and title

--- nettisivu/src/App.js
+++ nettisivu/src/App.js
@@ -1,8 +1,18 @@
+import MetsaImg from "images/metsa.png";
+
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col px-8 bg-midnight">
-      <div className="w-full text-yellow-500 font-bold text-6xl text-center mt-16">
-        Zombies
+    <div
+      className="h-screen w-screen flex flex-col px-8 bg-midnight"
+      // Custom style as a work-around to create transparent image background
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${MetsaImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="w-full text-lime-400 font-bold text-6xl text-center mt-16">
+        ZOMBIE VIOLATION
       </div>
     </div>
   );
