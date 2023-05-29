ba2e5a1f946ef4b3753c140b9f11ff6692203a45
Klicer <51763508+Klicer@users.noreply.github.com>
2023-05-29 22:05:25 +0300

    Add info about weapons and zombies to website

--- nettisivu/src/App.js
+++ nettisivu/src/App.js
@@ -1,4 +1,6 @@
 import MetsaImg from "images/metsa.png";
+import PistolImg from "images/pistol.png";
+import ZombieImg from "images/zombie.png";
 
 function App() {
   return (
@@ -13,6 +15,20 @@ function App() {
     >
       <div className="w-full text-lime-400 font-bold text-6xl text-center mt-16">
         NIGHTFALL
+      </div>
+      <div className="w-full flex justify-center mt-44 gap-44">
+        <div className="p-4 bg-midnight/70 rounded-xl flex flex-col gap-8 items-center">
+          <img src={PistolImg} width="200px" />
+          <div className="w-[320px] text-white">
+            Use multiple different and unique weapons
+          </div>
+        </div>
+        <div className="p-4 bg-midnight/70 rounded-xl flex flex-col gap-8 items-center">
+          <img src={ZombieImg} width="200px" />
+          <div className="w-[320px] text-white">
+            Eliminate as many zombies as possible without dying
+          </div>
+        </div>
       </div>
     </div>
   );
