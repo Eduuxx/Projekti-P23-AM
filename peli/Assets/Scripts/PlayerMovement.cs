a9916ed979d484c48257ea9996baa46c1a3e7dca
Finnbyte <eelpy@protonmail.com>
2023-05-29 22:30:10 +0300

    Fix jumping

--- peli/Assets/Scripts/PlayerMovement.cs
+++ peli/Assets/Scripts/PlayerMovement.cs
@@ -60,28 +60,13 @@ public class PlayerMovement : MonoBehaviour
         moveDirection.z = Input.GetAxis("Vertical");
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
         playerVelocity.y += gravity * Time.deltaTime;
-        if (isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = -2f;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
 
-    public void ProcessMove(Vector2 input)
-    {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-        playerVelocity.y += gravity * Time.deltaTime;
-        if (isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = -2f;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
-
-    public void Jump()
-    {
-        if (isGrounded)
-        {
+        if (isGrounded && Input.GetKey("space")) {
             playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
         }
+
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
