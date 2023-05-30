6c453beb0be8d1a5d5e5b425e8c997414244aff9
Finnbyte <eelpy@protonmail.com>
2023-05-30 12:47:34 +0300

    Implement medkits

--- peli/Assets/Scripts/PlayerBehaviour.cs
+++ peli/Assets/Scripts/PlayerBehaviour.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] HealBarScript _healthBar;
+    private Entity playerEntity;
 
     void Start()
     {
-        
+        playerEntity = GetComponent<Entity>();
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Using a medkit
+        if (Input.GetKeyDown("4"))
         {
-            PlayerTakeDmg(20);
-            Debug.Log(GameManager.gameManager._playerHealth.Health);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            PlayerHeal(10);
-            Debug.Log(GameManager.gameManager._playerHealth.Health);
+            // TODO SIMPLE INVENTORY FOR MEDKITS
+            Debug.Log("4 pressed");
+            // Check if using a medkit would heal for too much hp
+            if (playerEntity.HP + 30 > playerEntity.maxHP)
+            {
+                // In that case just set HP to max hp
+                playerEntity.HP = playerEntity.maxHP;
+            }
+            else
+            {
+                playerEntity.HP += 30;
+            }
         }
     }
 
