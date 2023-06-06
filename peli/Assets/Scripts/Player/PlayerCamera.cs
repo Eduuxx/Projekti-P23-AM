a991ba906727476f68ebecdd71464e3c62a8ac12
Finnbyte <eelpy@protonmail.com>
2023-06-06 03:14:05 +0300

    Fix camera bug affecting usage of main menu

--- peli/Assets/Scripts/Player/PlayerCamera.cs
+++ peli/Assets/Scripts/Player/PlayerCamera.cs
@@ -16,8 +16,8 @@ public class PlayerCamera : MonoBehaviour
     void Start()
     {
         // Lock and Hide the Cursor
-        Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
