9d40d02cdeee1ea9e323fc05c2bc001389027deb
Eduard Osmani <102226062+Eduuxx@users.noreply.github.com>
2023-06-06 08:21:17 +0300

    Add files via upload

---
+++ peli/Assets/Scripts/SoundManagement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepScript : MonoBehaviour
+{
+    public AudioSource footstepsSound, sprintSound, jumpSound;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+        
+        {
+            footstepsSound.enabled = false;
+            sprintSound.enabled = true;
+               
+        }
+            else
+            {
+                footstepsSound.enabled = true;
+                sprintSound.enabled = false;
+               
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+                jumpSound.enabled = true;
+            }
+            else
+            {
+  
+                jumpSound.enabled = false;
+
+            }
+            
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+            sprintSound.enabled = false;
+            jumpSound.enabled = false;
+        }
+    }
+}
